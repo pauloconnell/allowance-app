5f7ab678193867ae36faa3340748006b25450cda
Paul O'Connell <p_ollie@hotmail.com>
2026-04-26 18:26:26 -0700

    feat: Add Navbar WIP

--- src/app/protectedPages/[familyId]/layout.tsx
+++ src/app/protectedPages/[familyId]/layout.tsx
@@ -1,17 +1,26 @@
 import FamilyProvider from '@/components/FamilyProvider';
 import { ReactNode } from 'react';
+import { TabBar } from '@/components/TabBar/TabBar';
+
 
 interface FamilyLayoutProps {
    children: ReactNode;
-   params: Promise<{ familyId: string }>;
+   params: { familyId: string };
 }
 
 export default async function FamilyLayout({ children, params }: FamilyLayoutProps) {
-   const { familyId } = await params;
+   const { familyId } = params;
 
    return (
       <FamilyProvider familyId={familyId}>
-         {children}
+
+        <div className="min-h-screen flex flex-col">
+         <TabBar familyId={familyId} />
+
+         <main className="flex-1">
+          {children}
+        </main>
+      </div>
       </FamilyProvider>
    );
 }
